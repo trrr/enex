33879cd7df5bd29149aa052af52396915236a33e
trrr <onegalgo@gmail.com>
2014-01-24 15:44:23 +0400

    Added exercise model spec

--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -2,16 +2,28 @@ require 'spec_helper'
 
 describe Exercise do
 
-  it "is valid " do
-    expect(build(:exercise)).to be_valid
+  it "is valid with valid data" do
+    expect(build :exercise).to be_valid
   end
 
   it "validates presence of name" do
-    expect(build(:exercise, name: "")).not_to be_valid
+    expect(build :exercise, name: "").not_to be_valid
   end
 
   it "validates presence of description" do
-    expect(build(:exercise, description: "")).not_to be_valid
+    expect(build :exercise, description: "").not_to be_valid
   end
 
+  describe "making relations" do
+    let(:exercise) {build :exercise}
+    let(:text) {build :text}
+
+    before do
+      exercise.texts << text
+    end
+
+    it "returns exercise texts" do
+      expect(exercise.texts).to eq [text]
+    end
+  end
 end
