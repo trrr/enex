77faf7385e5df72e45ccf6937ba8c1ab63337ac8
trrr <onegalgo@gmail.com>
2014-01-24 16:29:57 +0400

    Added Word model spec

--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe Word do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:exercise) {build :exercise}
+  let(:word) {build :word, exercise: exercise}
+
+  before { exercise.save! }
+
+  describe "validations" do
+    it "is valid with valid data" do
+      expect(word).to be_valid
+    end
+
+    it "validates presence of body" do
+      expect(build :word, body: "").not_to be_valid
+    end
+
+
+    it "validates presence of exercise_id" do
+      expect(build :word, exercise: nil).not_to be_valid
+    end
+
+  end
+
+  describe "making relations" do
+
+    it "returns text exercises" do
+      expect(word.exercise).to eq exercise
+    end
+  end
 end
