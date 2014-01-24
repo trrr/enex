8e8997aa1fb923675ec4711872a45af6454566e8
trrr <onegalgo@gmail.com>
2014-01-24 15:45:57 +0400

    Added Text model spec

--- spec/models/text_spec.rb
+++ spec/models/text_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe Text do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid data" do
+    expect(build :text).to be_valid
+  end
+
+  it "validates presence of name" do
+    expect(build :text, body: "").not_to be_valid
+  end
+
+  describe "making relations" do
+    let(:exercise) {build :exercise}
+    let(:text) {build :text}
+
+    before do
+      text.exercises << exercise
+    end
+
+    it "returns text exercises" do
+      expect(text.exercises).to eq [exercise]
+    end
+  end
 end
