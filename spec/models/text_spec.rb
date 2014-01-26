cf512fd2a09fe26c377cd994cfdc4636e2a90fa1
name <email@email.com>
2014-01-26 22:41:49 +0400

    test search method in text

--- spec/models/text_spec.rb
+++ spec/models/text_spec.rb
@@ -9,9 +9,11 @@ describe Text do
     expect(build :text, body: "").not_to be_valid
   end
 
+  let(:exercise) {build :exercise}
+  let(:text) {build :text}
+  let(:word) {build :word, exercise: exercise}
+
   describe "making relations" do
-    let(:exercise) {build :exercise}
-    let(:text) {build :text}
 
     before do
       text.exercises << exercise
@@ -20,5 +22,18 @@ describe Text do
     it "returns text exercises" do
       expect(text.exercises).to eq [exercise]
     end
+
   end
+
+  describe "#search" do
+
+    it "searchs for a word in the body" do
+      expect(text.search(word)).to be_true
+    end
+
+    it "tests edge cases" do 
+      pending "Make edge cases tests"
+    end
+  end
+
 end
