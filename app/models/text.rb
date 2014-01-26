e1e40739bb8e88a1fb60c6c1812c94eb79eabdf3
name <email@email.com>
2014-01-26 22:42:41 +0400

    search in text

--- app/models/text.rb
+++ app/models/text.rb
@@ -2,4 +2,8 @@ class Text < ActiveRecord::Base
   has_many :exercises, through: :exercise_texts
   has_many :exercise_texts
   validates :body, presence: true
+
+  def search(word)
+    self.body.downcase.include? word.body.downcase
+  end
 end
