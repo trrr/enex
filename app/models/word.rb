a2ae5dfe036e83fd2634e809d012deaf3d944bed
trrr <onegalgo@gmail.com>
2014-01-27 05:59:58 +0400

    Deleted unnecessary code.

--- app/models/word.rb
+++ app/models/word.rb
@@ -3,38 +3,18 @@ class Word < ActiveRecord::Base
   validates :exercise_id, :body, presence: true
   before_save :make_relations
 
-    def texts
-      if self.exercise.texts.count > 0
-        Text.where("id NOT IN (?)", self.exercise.texts.map(&:id)).search(self.body)
-      else
-        Text.search(self.body)
-      end
+  def texts
+    # Exclude any existing relations if any, else full table search.
+    if self.exercise.texts.count > 0
+      Text.where("id NOT IN (?)", self.exercise.texts.map(&:id)).search(self.body)
+    else
+      Text.search(self.body)
     end
+  end
 
   private 
 
     def make_relations
-      texts.each do |t|
-        self.exercise.texts << t
-      end
+      texts.each  { |t| self.exercise.texts << t }
     end
-
-    # Create relation Excersice << Text if Text includes this word. 
-    # Now we can do exercise.texts and get all texts with exersice words.
-    # def make_relations
-    #   #TODO: refactor that crap
-    #   if texts != []
-    #     search_through_texts_and_make_relation(texts)
-    #   elsif texts == [] && self.exercise.texts == []
-    #     search_through_texts_and_make_relation(Text.all)
-    #   else
-    #     return
-    #   end
-    # end
-
-    # def search_through_texts_and_make_relation(texts)
-    #   texts.each do |t|
-    #     self.exercise.texts << t if t.includes?(self)
-    #   end
-    # end
 end
