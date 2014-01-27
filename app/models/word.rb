2a366e462be4e24a9029ce474116dec5f3f74fc5
trrr <onegalgo@gmail.com>
2014-01-27 06:32:24 +0400

    made texts method private

--- app/models/word.rb
+++ app/models/word.rb
@@ -3,16 +3,18 @@ class Word < ActiveRecord::Base
   validates :exercise_id, :body, presence: true
   before_save :make_relations
 
-  def texts
-    # Exclude any existing relations if any, else full table search.
-    if self.exercise.texts.count > 0
-      Text.where("id NOT IN (?)", self.exercise.texts.map(&:id)).search(self.body)
-    else
-      Text.search(self.body)
-    end
-  end
 
   private 
+
+    # Exclude any existing relations from the search,
+    # if no relations exists make full table search.
+    def texts    
+      if self.exercise.texts.count > 0
+        Text.where("id NOT IN (?)", self.exercise.texts.map(&:id)).search(self.body)
+      else
+        Text.search(self.body)
+      end
+    end
 
     def make_relations
       texts.each  { |t| self.exercise.texts << t }
